922cd0b594a7645194e6258136cecd2d2df30d39
LILAY <contact@lilay.dev>
2025-09-29 16:42:12 +0700

    Long time no see

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,14 @@
 export default function About() {
   const name: string = "LILAY";
   const greeting: string = `The name is `;
-  const blob0: string = ` ! I'm a Computer Science student with a passion for Linux and homelabbing. Currently based in Kampong Speu, Cambodia, I enjoy creating and learning new things. With a solid foundation in programming languages such as `;
-  const blob1: string = `, I'm also interested in containerization using `;
-  const blob2: string = `servers. I'm always looking to expand my skillset.`;
+  const blob0: string = ` ! I'm a Computer Science student who's totally hooked on Linux and homelabbing. Hailing from Kampong Speu, Cambodia, I live for the rush of creating and uncovering new adventures. From open-source projects to tinkering with hardware in my home setup. My go-to languages include `;
+  const blob1: string = ` and I utilize containerization with `;
+  const blob2: string = `servers.`;
+  const blob3: string = `Beyond the code, I'm constantly chasing what interests me (not just tech) and sharing knowledge with friends and the community. I'm pumped to join forces on projects that shake things up and make a real difference!`;
   return (
     <>
       <div className="mx-auto max-w-[1400px] flex flex-col gap-4 h-full">
-        <div className="flex flex-col justify-start w-full pt-8 mb:w-2/6 sm:pl-10">
+        <div className="flex flex-col justify-start w-full pt-8 sm:pt-32 mb:w-2/6 sm:pl-10">
           <h1 className="text-[#fabd2f] text-[24px] md:text-[34px] sm:text-[28px] font-patua_one">
             About Me
           </h1>
@@ -19,18 +20,15 @@ export default function About() {
             <span className="text-[#f7df1e]"> JS </span> /
             <span className="text-[#377cc8]"> TS </span>, and frameworks like
             <span className="text-[#81e0ff]"> React </span> /
-            <span className="text-[#ffffff]"> NextJS </span>
-            {blob1}
+            <span className="text-[#ffffff]"> NextJS </span>,{blob1}
             <span className="text-[#0b9dc8]">Docker</span> and
             <span className="text-[#ff9c08]"> cloud </span>
             {blob2}
           </p>
-          <h1 className="text-[#d3869b] text-[24px] md:text-[34px] sm:text-[28px] font-patua_one pt-4">
-            Technologies
-          </h1>
+          <p className="text-[#fbf1c7] text-[18px] md:text-[24px] sm:text-[24px] font-patua_one pt-4">
+            {blob3}
+          </p>
         </div>
-
-        <div className="flex flex-col h-full md:flex-row"></div>
       </div>
     </>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,14 @@
 export default function Projects() {
   return (
-    <>
-      <h1 className="text-white">Hi from Projects</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-white text-2xl md:text-4xl mb-4 font-patua_one">
+        Under Construction :)
+      </h1>
+      <img
+        src="https://i.giphy.com/XZrUw9cTqJ0vi8bmV9.webp"
+        alt="Under Construction GIF"
+        className="max-w-xs md:max-w-md"
+      />
+    </div>
   );
 }
